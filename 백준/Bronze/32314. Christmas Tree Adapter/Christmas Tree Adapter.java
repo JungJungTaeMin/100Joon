a1754fab675276b7f7d5132691567a4b86126ae6
JungJungTaeMin <jtm45890821@gmail.com>
2025-11-06 11:09:38 +0900

    [Bronze V] Title: Christmas Tree Adapter, Time: 84 ms, Memory: 12820 KB -BaekjoonHub

---
+++ 백준/Bronze/32314. Christmas Tree Adapter/Christmas Tree Adapter.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        int w = sc.nextInt();
+        int v = sc.nextInt();
+
+
+        int adapter_ampere = w / v;
+
+        if (adapter_ampere >= a) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+
+        sc.close();
+    }
+}
