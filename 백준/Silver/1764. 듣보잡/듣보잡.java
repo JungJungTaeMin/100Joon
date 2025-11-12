b5e56a892e01691e2ea646fde429531035c47a44
JungJungTaeMin <jtm45890821@gmail.com>
2025-11-12 13:38:47 +0900

    [Silver IV] Title: 듣보잡, Time: 232 ms, Memory: 26244 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        HashSet<String> noN = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            noN.add(br.readLine());
+        }
+
+        ArrayList<String> result = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            String name = br.readLine();
+            if (noN.contains(name)) {
+                result.add(name);
+            }
+        }
+
+        Collections.sort(result);
+
+        sb.append(result.size()).append('\n');
+        for (String s : result) {
+            sb.append(s).append('\n');
+        }
+
+        System.out.print(sb);
+    }
+}
